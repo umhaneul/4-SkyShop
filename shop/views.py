f0acc3fd517498700631e3bfc4dc8276f73850dc
s2018w09 <s2018w09@e-mirim.hs.kr>
2020-09-04 15:48:54 +0900

    shop/views product_in_category

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from shop.models import Category, Product
+
+
+def product_in_category(request) :
+    categories = Category.objects.all()
+    products = Product.objects.all()
+
+    return render(request, 'shop/list.html', {'categories': categories, 'products':products})
